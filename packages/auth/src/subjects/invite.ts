81c4c505e83a9c39061624793d47273621854cec
ludiemert <lucianadiemert@gmail.com>
2024-05-14 23:17:47 -0300

    feat: configure resource billing and invite for peoject with zod

---
+++ packages/auth/src/subjects/invite.ts
@@ -0,0 +1,16 @@
+//convites
+
+import { z } from 'zod'
+
+export const inviteSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),  
+    z.literal('delete'), //apaga um convite que foi enviado
+    
+  ]),
+  z.literal('Invite'),
+])
+
+export type InviteSubject = z.infer<typeof inviteSubject>
