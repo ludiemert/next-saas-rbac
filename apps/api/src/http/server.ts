4ab6526be789dff5706accc83c2fe3fe5ca1fae2
ludiemert <lucianadiemert@gmail.com>
2024-08-05 22:14:47 -0300

    add route update-members.ts backend

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -31,6 +31,7 @@ import { getProject } from './routes/projects/get-project';
 import { getProjects } from './routes/projects/get-projects';
 import { updateProject } from './routes/projects/update-project';
 import { getMembers } from './routes/members/get-members';
+import { updateMember } from './routes/members/update-member';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -91,6 +92,7 @@ app.register(getProjects);
 app.register(updateProject);
 
 app.register(getMembers);
+app.register(updateMember);
 
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
