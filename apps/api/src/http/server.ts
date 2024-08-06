2788c54eaddaa293f0223eea1a492f9b9debca7e
ludiemert <lucianadiemert@gmail.com>
2024-08-06 20:17:08 -0300

    add route remove-member the project

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -32,6 +32,7 @@ import { getProjects } from './routes/projects/get-projects';
 import { updateProject } from './routes/projects/update-project';
 import { getMembers } from './routes/members/get-members';
 import { updateMember } from './routes/members/update-member';
+import { removeMember } from './routes/members/remove-member';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -93,6 +94,7 @@ app.register(updateProject);
 
 app.register(getMembers);
 app.register(updateMember);
+app.register(removeMember);
 
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
