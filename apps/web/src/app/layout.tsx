5439bdb11efd53d24aa03fdcd69222f220d855a6
ludiemert <lucianadiemert@gmail.com>
2024-08-15 20:11:33 -0300

    add project frontend - install next and configuration

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Project next-saas-rbac App",
+  description: "Project Trip - FullStack",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
