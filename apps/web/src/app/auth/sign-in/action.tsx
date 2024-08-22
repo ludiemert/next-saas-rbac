481ff7a74b552eb6baa35b1d2d61d4040bde9563
ludiemert <lucianadiemert@gmail.com>
2024-08-21 21:36:44 -0300

    add conection front and back - para conectar o front com o back nao sera usado o axios e sim o fetch do REACT vamos usar o KY eh mais leve e  ele usa as featire do axios => ele cria um AXIOS em cima da FETCH API do REACT

--- apps/web/src/app/auth/sign-in/action.tsx
+++ apps/web/src/app/auth/sign-in/action.tsx
@@ -1,5 +1,30 @@
 'use server'
 
+import ky from 'ky'
+
+const api = ky.create({
+  prefixUrl: 'http://localhost:3333',
+})
+
 export async function signInWithEmailAndPassword(data: FormData) {
-  console.log(Object.fromEntries(data))
+  // Extraindo os valores do FormData
+  const formData = Object.fromEntries(data.entries())
+  const { email, password } = formData
+
+  console.log(formData)
+
+  try {
+    const result = await api
+      .post('sessions/password', {
+        json: {
+          email,
+          password,
+        },
+      })
+      .json()
+
+    console.log(result)
+  } catch (error) {
+    console.error('Error during sign in:', error)
+  }
 }
