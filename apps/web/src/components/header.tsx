aecda9abf4e93ca654edef49e9e9eab61d116396
ludiemert <lucianadiemert@gmail.com>
2024-09-24 21:45:20 -0300

    instal pnpm dlx shadcn-ui@latest add dropdown-menu avatar for do Header front

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import iconIMG from '@/assets/icon.svg'
+import iconIMG from "@/assets/icon.svg";
 
 export function Header() {
-  return (
-    <div className="max-w[1200px] mx-auto flex items-center justify-between">
-      <div className="flex items-center gap-3">
-        <Image src={iconIMG} className="size-8 dark:invert" alt="Icon Dog" />
-      </div>
+	return (
+		<div className="max-w[1200px] mx-auto flex items-center justify-between">
+			<div className="flex items-center gap-3">
+				<Image src={iconIMG} className="size-8 dark:invert" alt="Icon Dog" />
+			</div>
 
-      <div className="flex items-center gap-4"></div>
-    </div>
-  )
+			<div className="flex items-center gap-4"></div>
+		</div>
+	);
 }
