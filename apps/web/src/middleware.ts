a5f936a26df190be2d15f80787fa42e662b91635
ludiemert <lucianadiemert@gmail.com>
2024-09-29 22:37:36 -0300

    create middleware.ts for route config

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+	//console.log("Helllo");
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - api (API routes)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+		 */
+		"/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+	],
+};
