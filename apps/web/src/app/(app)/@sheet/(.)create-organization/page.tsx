c0006bd0cab50a7990b6fa3240ae9ebcbd1a1eda
ludiemert <lucianadiemert@gmail.com>
2024-10-07 17:00:30 -0300

    add component intercepted-sheet-content for close modal create organization

--- apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -1,19 +1,18 @@
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { OrganizationForm } from "../../create-organization/organization-form";
+import { InterceptedSheetContent } from "@/components/intercepted-sheet-content";
 
 export default function CreateOrganization() {
   return (
     <Sheet defaultOpen>
-      <SheetContent>
+      <InterceptedSheetContent>
         <SheetHeader>
           <SheetTitle>Create organization🥳😍🤩</SheetTitle>
         </SheetHeader>
         <div className="py-4">
         <OrganizationForm />
         </div>
-
-      </SheetContent>
-
+      </InterceptedSheetContent>
     </Sheet>
   )
 }
